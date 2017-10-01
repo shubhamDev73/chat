5a86fe0091d86e169f82507e60577518f9924a0d
Shubham <shubham0209@gmail.com>
2017-10-01 23:30:36 +0530

    Added <br> to span

--- public/javascripts/chatSend.js
+++ public/javascripts/chatSend.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 		chat.html(function(x, oldHtml){
 			oldHtml += '			<span id="remove">\n'
 			oldHtml += '				<h3>'+$("#nickname").html()+'</h3>\n';
-			oldHtml += '				<p>'+text+'<span id="status">&nbsp;&nbsp;&nbsp;&nbsp;sending...</span></p></span><br>';
+			oldHtml += '				<p>'+text+'<span id="status">&nbsp;&nbsp;&nbsp;&nbsp;sending...</span></p><br><span>';
 			return oldHtml;
 		});
 		chat.scrollTop(chat[0].scrollHeight);
